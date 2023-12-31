bb521d1e023f255be6199e69e26f4a6026e144f2
garamond13 <98652255+garamond13@users.noreply.github.com>
2023-12-31 02:29:46 +0100

    bump version number before release
    - v2.0.2

--- SingleMenuScreenshot/version.h
+++ SingleMenuScreenshot/version.h
@@ -2,4 +2,4 @@
 
 inline constexpr int SMSS_VERSION_NUMBER_MAJOR{ 2 };
 inline constexpr int SMSS_VERSION_NUMBER_MINOR{ 0 };
-inline constexpr int SMSS_VERSION_NUMBER_PATCH{ 1 };
+inline constexpr int SMSS_VERSION_NUMBER_PATCH{ 2 };
